e44dd4159d79b4dc0477f42422a2584772a40d7f
Anton Miklis <antonmiklis@icloud.com>
2021-06-14 23:45:40 +0300

    feat: completed task1 task2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,32 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if prices.count == 0 {
+            return 0
+        }
+        var profit = 0
+        var pricesMutable = prices
+        var buyingPrices = [Int]()
+        var profitTemp = 23456
+        while profit != profitTemp || profitTemp == 0{
+            if pricesMutable.count == 0 {
+                print("break")
+                break
+            }
+            buyingPrices = []
+            profitTemp = profit
+            var highestPriceIndex = pricesMutable.lastIndex(of:pricesMutable.max()!);
+            var i = 0
+            var highestPrice = pricesMutable[highestPriceIndex!]
+            pricesMutable.remove(at: highestPriceIndex!)
+            while i < highestPriceIndex! {
+                buyingPrices.append(pricesMutable[0])
+                pricesMutable.remove(at: 0)
+                i += 1
+            }
+            profit += highestPriceIndex! * highestPrice - buyingPrices.reduce(0, +)
+            print(profit)
+        }
+        return profit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,60 @@
 import UIKit
 
+extension StringProtocol {
+    subscript(offset: Int) -> Character {
+        self[index(startIndex, offsetBy: offset)]
+    }
+}
+
 class MessageDecryptor: NSObject {
     
-    func decryptMessage(_ message: String) -> String {
-        return ""
+    func decryptMessage(_ originalMessage: String) -> String {
+        print(originalMessage.count)
+        var message = originalMessage
+        var decryptedMessage = ""
+        var messageBefore = ""
+        while messageBefore != message {
+            decryptedMessage = ""
+            var omit = 0
+            var index = 0
+            messageBefore = message
+            while index < message.count {
+                if message[index].isNumber {
+                    var multiplier = Int(String(message[index]))
+                    index += 2
+                    let startIndex = index
+                    omit = 0
+                    if multiplier == 0 {
+                        return ""
+                    }
+                    while multiplier != 0{
+                        if message[index] == "]" && omit == 0 {
+                            multiplier! -= 1
+                            if multiplier == 0 {
+                                break
+                            }
+                            index = startIndex
+                        }
+                        if message[index].isNumber {
+                            omit += 1
+                        }
+                        if message[index] == "]" {
+                            omit -= 1
+                        }
+                        decryptedMessage += String(message[index])
+                        index += 1
+                        if index == message.count {
+                            return ""
+                        }
+                    }
+                }
+                if message[index] != "]" && message[index] != "["{
+                    decryptedMessage += String(message[index])
+                }
+                index += 1
+            }
+            message = decryptedMessage
+        }
+        return decryptedMessage
     }
 }
